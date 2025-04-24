3c96674121c2f2cf4c379dd7f2d9e3fe7939742f
MrRiver69 <riversbenjamin5@gmail.com>
2025-04-24 19:10:02 +1000

    Initial commit

---
+++ Source/MultiMower/MultiMower.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MultiMower.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiMower, "MultiMower" );
+ 
---
+++ Source/MultiMower/MultiMowerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiMowerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMultiMowerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMultiMowerGameMode();
+};
+
+
+
